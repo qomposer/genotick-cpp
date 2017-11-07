e962f168c5dfc4680542c858b7d5e872033d9adb
xezon <pascalkross@gmail.com>
2017-11-07 22:07:17 +0100

    Fix pointer cast truncation for x64 in Console class

--- src/console.cpp
+++ src/console.cpp
@@ -2,7 +2,8 @@
 #include "console.h"
 #include <iostream>
 #include <Windows.h>
-#include <stdio.h>
+#include <cstdio>
+#include <cstdint>
 #include <io.h>
 #include <fcntl.h>
 
@@ -12,9 +13,9 @@ bool CConsole::Create()
 	{
 		::SetConsoleTitleA("Genotick Console");
 
-		*(iobuf*)stdout = *(iobuf*)::_fdopen(::_open_osfhandle((long)::GetStdHandle(STD_OUTPUT_HANDLE), _O_TEXT), "w");
-		*(iobuf*)stderr = *(iobuf*)::_fdopen(::_open_osfhandle((long)::GetStdHandle(STD_ERROR_HANDLE), _O_TEXT), "w");
-		*(iobuf*)stdin = *(iobuf*)::_fdopen(::_open_osfhandle((long)::GetStdHandle(STD_INPUT_HANDLE), _O_TEXT), "r");
+		*(iobuf*)stdout = *(iobuf*)::_fdopen(::_open_osfhandle((::std::intptr_t)::GetStdHandle(STD_OUTPUT_HANDLE), _O_TEXT), "w");
+		*(iobuf*)stderr = *(iobuf*)::_fdopen(::_open_osfhandle((::std::intptr_t)::GetStdHandle(STD_ERROR_HANDLE), _O_TEXT), "w");
+		*(iobuf*)stdin = *(iobuf*)::_fdopen(::_open_osfhandle((::std::intptr_t)::GetStdHandle(STD_INPUT_HANDLE), _O_TEXT), "r");
 		::setvbuf(stdout, NULL, _IONBF, 0);
 		::setvbuf(stderr, NULL, _IONBF, 0);
 		::setvbuf(stdin, NULL, _IONBF, 0);
